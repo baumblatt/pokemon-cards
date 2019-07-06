48417189479de098368f8f622e6f2bc0afc79b03
Bernado Baumblatt <baumblatt@gmail.com>
2019-07-06 16:53:02 -0300

    Criação de Componentes

--- src/app/pokemon/pokemon-routing.module.ts
+++ src/app/pokemon/pokemon-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PokemonDetailComponent} from './containers/pokemon-detail/pokemon-detail.component';
+import {PokemonListComponent} from './containers/pokemon-list/pokemon-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'list'},
+  {path: 'list', component: PokemonListComponent},
+  {path: 'detail', component: PokemonDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PokemonRoutingModule } from './pokemon-routing.module';
+import { PokemonListComponent } from './containers/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './containers/pokemon-detail/pokemon-detail.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PokemonListComponent, PokemonDetailComponent],
   imports: [
     CommonModule,
     PokemonRoutingModule
