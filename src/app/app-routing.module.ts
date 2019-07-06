dc5a722e714f6130663334a5e0e2314a5b3aef15
Bernado Baumblatt <baumblatt@gmail.com>
2019-07-06 16:42:07 -0300

    Criação de Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CoreModule} from './core/core.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'core'},
+  {path: 'core', loadChildren: () => CoreModule}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
